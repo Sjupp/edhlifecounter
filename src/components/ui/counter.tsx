3fa52d2e3192d1a72416dd540275233ce81c0efa
Emil Forsén <emilxforsen@gmail.com>
2024-06-17 18:52:26 +0200

    Added menu

--- src/components/ui/counter.tsx
+++ src/components/ui/counter.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { MinusIcon, PlusIcon } from "lucide-react";
 import { Button } from "./button";
-import React from "react";
-import { on } from "events";
 
 interface CounterProps {
   rot: string;
