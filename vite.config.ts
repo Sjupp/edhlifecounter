b05e0797e5418bb86c5bc1e565a330c0f65db3c4
Emil Forsén <emilxforsen@gmail.com>
2024-06-24 11:01:38 +0200

    Added dependencies to support react-window

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: true,
     rollupOptions: {
-      external: ['react-window']
-    }
+      external: ['react-window'],
+       onwarn(warning, defaultHandler) {
+        if (warning.code === 'SOURCEMAP_ERROR') {
+          return
+        }
+
+        defaultHandler(warning)
+      },
+    },
   },
   resolve: {
     alias: {
