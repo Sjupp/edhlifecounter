f0d397178f954d6d79b106fffa3b1f8cf2ff0976
Anton <a.e.lindkvist@gmail.com>
2024-07-11 23:08:20 +0200

    prettier

--- src/components/ui/playerSelect.tsx
+++ src/components/ui/playerSelect.tsx
@@ -6,7 +6,12 @@ import { Card, CardContent, CardHeader } from "./card";
 import { Input } from "./input";
 
 const { legendaryCreatures } = preprocessCards();
-import { playerData, updatePlayerData, updateCommanderData, Player } from "@/data/players/playerData";
+import {
+  playerData,
+  updatePlayerData,
+  updateCommanderData,
+  Player,
+} from "@/data/players/playerData";
 
 interface playerSelectProps {
   numPlayers: number;
@@ -17,8 +22,7 @@ export default function PlayerSelect({
   numPlayers,
   onConfirm,
 }: playerSelectProps) {
-  
-const [players, setPlayers] = useState<Player[]>(playerData);
+  const [players, setPlayers] = useState<Player[]>(playerData);
 
   const updatePlayer = (id: number, newName: string) => {
     setPlayers((prevPlayers) =>
@@ -40,33 +44,36 @@ const [players, setPlayers] = useState<Player[]>(playerData);
 
   return (
     <>
-
-
-<div className="flex flex-col items-center justify-center h-full w-full overflow-auto">
-    <Card className="flex flex-col justify-center w-2/3 items-center">
-
-      <div className="flex flex-wrap gap-4 justify-center items-center pointer-events-auto p-4">
-        {players.map((i) => (
-            
-            <Card key={i.id}>
-            <CardHeader>{i.name}</CardHeader>
-            <CardContent className="">
-            <Input type="name" placeholder={i.name} className="my-2" onChange={(e) => updatePlayer(i.id, e.target.value)}></Input>
-              <Combobox
-                legendaryCreatures={legendaryCreatures}
-                playerId={i.id}
-                updateCommander={updateCommander}
-                ></Combobox>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-      <div>
-        <Button onClick={() => onConfirm(numPlayers)}>Start Game</Button>
-        <Button onClick={() => console.log(playerData)}>Check Players</Button>
-      </div>
+      <div className="flex flex-col items-center justify-center h-full w-full overflow-auto">
+        <Card className="flex flex-col justify-center w-2/3 items-center">
+          <div className="flex flex-wrap gap-4 justify-center items-center pointer-events-auto p-4">
+            {players.map((i) => (
+              <Card key={i.id}>
+                <CardHeader>{i.name}</CardHeader>
+                <CardContent className="">
+                  <Input
+                    type="name"
+                    placeholder={i.name}
+                    className="my-2"
+                    onChange={(e) => updatePlayer(i.id, e.target.value)}
+                  ></Input>
+                  <Combobox
+                    legendaryCreatures={legendaryCreatures}
+                    playerId={i.id}
+                    updateCommander={updateCommander}
+                  ></Combobox>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+          <div>
+            <Button onClick={() => onConfirm(numPlayers)}>Start Game</Button>
+            <Button onClick={() => console.log(playerData)}>
+              Check Players
+            </Button>
+          </div>
         </Card>
-        </div>
+      </div>
     </>
   );
 }
--- src/data/players/playerData.tsx
+++ src/data/players/playerData.tsx
@@ -1,46 +1,47 @@
-
 export interface Player {
-    id: number,
-    name: string,
-    commander: string;
+  id: number;
+  name: string;
+  commander: string;
 }
 
-
 export const playerData: Player[] = [
-{
+  {
     id: 1,
     name: "Player 1",
     commander: "",
-}, {
+  },
+  {
     id: 2,
     name: "Player 2",
     commander: "",
-}, {
-    id: 3, 
+  },
+  {
+    id: 3,
     name: "Player 3",
     commander: "",
-}, {
+  },
+  {
     id: 4,
     name: "Player 4",
     commander: "",
-}, {
+  },
+  {
     id: 5,
     name: "Player 5",
     commander: "",
-}
+  },
 ];
 
 export const updatePlayerData = (id: number, newName: string) => {
-    const player = playerData.find(player => player.id === id)
-    if (player) {
-        player.name = newName;
-    }
+  const player = playerData.find((player) => player.id === id);
+  if (player) {
+    player.name = newName;
+  }
 };
 
 export const updateCommanderData = (id: number, newCommander: string) => {
-    const player = playerData.find(player => player.id === id)
-    if (player) {
-        player.commander = newCommander;
-    }
+  const player = playerData.find((player) => player.id === id);
+  if (player) {
+    player.commander = newCommander;
+  }
 };
-
